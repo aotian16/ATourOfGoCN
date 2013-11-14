00caa292c94946661b286125c0037fd9ba80e0b7
aotian16 <tj861216@163.com>
2013-11-14 23:03:18 +0900

    new

---
+++ page5/main.go
@@ -0,0 +1,16 @@
+package main
+
+// 用括号组合导入包
+import (
+    "fmt"
+    "math"
+)
+
+// 也可以用,效果一样
+// import "fmt"
+// import "math"
+
+func main() {
+    fmt.Printf("Now you have %g problems.",
+        math.Nextafter(2, 3))
+}
