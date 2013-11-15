2188667021ba68088a2ddfeb590136caf218440a
aotian16 <aotian16@gmail.com>
2013-11-15 15:12:17 +0900

    new

---
+++ page34/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+func main() {
+    // for 循环的 range 格式可以对 slice 或者 map 进行迭代循环。
+    for i, v := range pow {
+        fmt.Printf("2**%d = %d\n", i, v)
+    }
+}
