87844277e5d450b67b5e5316ff466eacb563b1fe
aotian16 <aotian16@gmail.com>
2013-11-18 13:25:23 +0900

    new

---
+++ page67/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+/*
+select 语句使得一个 goroutine 在多个通讯操作上等待。
+select 会阻塞，直到条件分支中的某个可以继续执行，这时就会执行那个条件分支。当多个都准备好的时候，会随机选择一个。
+*/
+
+func fibonacci(c, quit chan int) {
+    x, y := 0, 1
+    for {
+        select {
+        case c <- x:
+            x, y = y, x+y
+        case <-quit:
+            fmt.Println("quit")
+            return
+        }
+    }
+}
+
+func main() {
+    c := make(chan int)
+    quit := make(chan int)
+    go func() {
+        for i := 0; i < 10; i++ {
+            fmt.Println(<-c)
+        }
+        quit <- 0
+    }()
+    fibonacci(c, quit)
+}
